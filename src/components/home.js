3f026bd0d57a6e874a44287320ce79df49aceb76
sujith <sujithsuresh246@gmail.com>
2020-05-02 14:48:31 +0530

    hero content added

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Image from "../components/image"
+
+import '../components/scss/home.scss'
+const Home = () => {
+  return (
+    <div className="hero-wrapper">
+      <div className="details">
+        <h1>Hi, I'am Sujith</h1>
+        <p>Iam a web developer and creative designer.</p>
+      </div>
+      <div className="intro-card" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </div>
+  )
+}
+
+export default Home
